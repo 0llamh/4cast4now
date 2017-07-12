039fc8e962bcb4078ba1f4f27fbad2a658266526
Cameron Smith <smith.paulcameron@gmail.com>
2017-07-12 17:44:07 -0400

    cleanup

--- app/src/main/java/com/example/ollamh/proj1/MainActivity.java
+++ app/src/main/java/com/example/ollamh/proj1/MainActivity.java
@@ -35,8 +35,7 @@ public class MainActivity extends AppCompatActivity {
         // Given State & City, we can hit our API
         API_URL = "http://api.wunderground.com/api/" + API_KEY + "/conditions/q/" + state + "/" + city + ".json"; // + STATE_NAME/CITY_NAME.json"
         JSONObject api = new JSONObject(API_URL);
-        String temp = api.getJSONObject("current_observations").getString("temp_f");
-        return temp;
+        return api.getJSONObject("current_observations").getString("temp_f");
     }
 
 }
