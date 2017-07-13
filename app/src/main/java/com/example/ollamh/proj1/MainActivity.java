a80014c2e5dbca1300eebbcdc205c41ca02a8c92
CarleeBerry <cfb13b@my.fsu.edu>
2017-07-13 17:17:52 -0400

    added severe weather notifications

--- app/src/main/java/com/example/ollamh/proj1/MainActivity.java
+++ app/src/main/java/com/example/ollamh/proj1/MainActivity.java
@@ -1,19 +1,34 @@
 package com.example.ollamh.proj1;
 
+import android.app.AlarmManager;
+import android.app.PendingIntent;
+import android.app.TaskStackBuilder;
+import android.content.Context;
+import android.icu.util.GregorianCalendar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.NotificationCompat;
+import android.view.View;
 import android.widget.TextView;
+import android.content.Intent;
+import java.util.Date;
 
 import org.json.*;
+import android.app.NotificationManager;
+
+import static android.app.TaskStackBuilder.create;
 
 
 public class MainActivity extends AppCompatActivity {
     GifView gifView;
     TextView temperatureView;
+    String state="Fairfax";
+    String city="Virginia";
 
     final private String API_KEY = "3b7b12e3bec57d6c";
     private String API_URL;
 
+    NotificationManager notificationManager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,12 +38,18 @@ public class MainActivity extends AppCompatActivity {
         gifView = (GifView) findViewById(R.id.gef_view);
         temperatureView = (TextView) findViewById(R.id.temperature);
 
+        try {
+            temperatureView.setText(getTemperature(state,city));
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
 
         // TODO: GET STATE & CITY STRING VALUES
         //      then, pass into getTemperature(state, city) function
         //
         // temperatureView.setText(getTemperature(state, city));
 
+        sendIntent();
     }
 
     protected String getTemperature(String state, String city) throws JSONException {
@@ -38,4 +59,21 @@ public class MainActivity extends AppCompatActivity {
         return api.getJSONObject("current_observations").getString("temp_f");
     }
 
+
+
+
+    public void sendIntent(){
+
+
+
+        Intent intent = new Intent();
+        intent.setAction("com.example.ollamh.proj1");
+        intent.putExtra("state",state);
+        intent.putExtra("city",city);
+
+
+
+        sendBroadcast(intent);
+
+    }
 }
---
+++ app/src/main/java/com/example/ollamh/proj1/notificationReceiver.java
@@ -0,0 +1,142 @@
+package com.example.ollamh.proj1;
+
+import android.app.AlarmManager;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.support.v7.app.NotificationCompat;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.Date;
+
+/**
+ * Created by carleneberry on 7/13/17.
+ */
+
+public class notificationReceiver extends BroadcastReceiver {
+    final private String API_KEY = "3b7b12e3bec57d6c";
+    private String API_URL;
+    private String severeWeather = "Severe Weather Alert";
+    private int isAlert= 0;
+    private String description="";
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+
+
+        String city=intent.getStringExtra("city");
+        String state=intent.getStringExtra("state");
+
+        try {
+            severeWeather=getWeatherAlert(state, city);
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+
+
+        //https://stackoverflow.com/questions/11434056/how-to-run-a-method-every-x-seconds
+
+            Date time = new Date(System.currentTimeMillis());
+            PendingIntent pendingIntent = PendingIntent.getBroadcast(context, 0, intent, PendingIntent.FLAG_CANCEL_CURRENT);
+
+            AlarmManager interval = (AlarmManager) context.getSystemService(Context.ALARM_SERVICE);
+
+            interval.setRepeating(AlarmManager.RTC_WAKEUP, time.getTime(), AlarmManager.INTERVAL_FIFTEEN_MINUTES, pendingIntent);
+
+            String title = translate();
+            NotificationCompat.Builder notificationBuilder = (NotificationCompat.Builder) new NotificationCompat.Builder(context)
+                    .setContentTitle(title)
+                    .setContentText(description)
+                    .setTicker("Severe Weather Alert")
+                    .setSmallIcon(R.drawable.ic_weather_alert);
+
+            notificationBuilder.setContentIntent(pendingIntent);
+
+
+            notificationBuilder.setDefaults(NotificationCompat.DEFAULT_VIBRATE);
+            notificationBuilder.setAutoCancel(true);
+
+            NotificationManager manager = (NotificationManager) context.getSystemService(Context.NOTIFICATION_SERVICE);
+            manager.notify(1, notificationBuilder.build());
+
+
+
+
+
+    }
+
+
+    protected String getWeatherAlert(String state, String city) throws JSONException {
+        // Given State & City, we can hit our API
+        API_URL = "http://api.wunderground.com/api/" + API_KEY + "/alerts/q/" + state + "/" + city + ".json"; // + STATE_NAME/CITY_NAME.json"
+
+        JSONObject api = new JSONObject(API_URL);
+        severeWeather = api.getJSONObject("alerts").getString("type");
+        isAlert = api.getJSONObject("alerts").getInt("alerts");
+        description=api.getJSONObject("alerts").getString("message");
+        return api.getJSONObject("alerts").getString("type");
+
+
+    }
+
+
+    String translate() {
+
+                String weather = severeWeather;
+
+        switch (severeWeather) {
+            case "HUR":
+                weather="New Hurricane Local Statement";
+                break;
+            case "TOR":
+                weather="Tornado Warning";
+                break;
+            case "TOW":
+                weather="Tornado Watch";
+                break;
+            case "WRN":
+                weather="Severe Thunderstorm Warning";
+                break;
+            case "SEW":
+                weather="Severe Thunderstorm Watch";
+                break;
+            case "WIN":
+                weather="Winter Weather Advisory";
+                break;
+            case "FLO":
+                weather="Flood Warning";
+                break;
+            case "WAT":
+                weather="Flood Watch";
+                break;
+            case "WND":
+                weather="High Wind Advisory";
+                break;
+            case "HEA":
+                weather="Heat Advisory";
+                break;
+            case "FOG":
+                weather="Dense Fog Advisory";
+                break;
+            case "VOL":
+                weather="Volcanic Activity Statement";
+                break;
+            case "FIR":
+                weather="Fire Weather Advisory";
+                break;
+            case "HWW":
+                weather="Hurricane Wind Warning";
+                break;
+            default: break;
+        }
+
+
+        return weather;
+    }
+
+    }
+
