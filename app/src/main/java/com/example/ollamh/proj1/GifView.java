41f2b9e2b1de321586de53fec0324a33e60e47d2
Cameron Smith <smith.paulcameron@gmail.com>
2017-07-16 15:51:30 -0400

    updates gifview based on conditions

--- app/src/main/java/com/example/ollamh/proj1/GifView.java
+++ app/src/main/java/com/example/ollamh/proj1/GifView.java
@@ -40,8 +40,6 @@ public class GifView extends View{
 
     private void init(Context context) {
         setFocusable(true);
-        gifInputStream = context.getResources().openRawResource(+R.drawable.sunny);
-        gifMovie = Movie.decodeStream(gifInputStream);
         movieWidth = gifMovie.width();
         movieHeight = gifMovie.height();
         movieDuration = gifMovie.duration();
@@ -53,7 +51,42 @@ public class GifView extends View{
     }
 
     public void setGifMovie(String condition){
-        gifInputStream = getContext().getResources().openRawResource(+R.drawable.stormy);
+        switch (condition) {
+            case "Light Drizzle": case "Heavy Drizzle": case "Drizzle":
+            case "Light Rain": case "Heavy Rain":case "Rain":
+            case "Light Rain Mist":case "Heavy Rain Mist":case "Rain Mist":
+            case "Light Rain Showers":case "Heavy Rain Showers":case "Rain Showers":
+                gifInputStream = getContext().getResources().openRawResource(+R.drawable.rainy);
+                break;
+            case "Light Fog":case "Heavy Fog":case "Fog":case "Light Fog Patches":case "Heavy Fog Patches":
+            case "Fog Patches":case "Patches of Fog":case "Shallow Fog":case "Partial Fog":
+                gifInputStream = getContext().getResources().openRawResource(+R.drawable.foggy);
+                break;
+            case "Overcast":case "Mostly Cloudy":
+                gifInputStream = getContext().getResources().openRawResource(+R.drawable.overcasty);
+                break;
+            case "Partly Cloudy":case "Scattered Clouds":
+                gifInputStream = getContext().getResources().openRawResource(+R.drawable.partly_cloudy);
+                break;
+            case "Light Snow":case "Heavy Snow":case "Snow":case "Light Snow Grains":case "Heavy Snow Grains":
+            case "Snow Grains":case "Light Low Drifting Snow":case "Heavy Low Drifting Snow":case "Low Drifting Snow":
+            case "Light Blowing Snow":case "Heavy Blowing Snow":case "Blowing Snow":case "Light Snow Showers":
+            case "Heavy Snow Showers":case "Snow Showers":case "Light Snow Blowing Snow Mist":
+            case "Heavy Snow Blowing Snow Mist":case "Snow Blowing Snow Mist":
+                gifInputStream = getContext().getResources().openRawResource(+R.drawable.snowy);
+                break;
+            case "Light Thunderstorm":case "Heavy Thunderstorm":case "Thunderstorm":case "Light Thunderstorms and Rain":
+            case "Heavy Thunderstorms and Rain":case "Thunderstorms and Rain":case "Light Thunderstorms and Snow":
+            case "Thunderstorms and Snow":case "Light Thunderstorms and Ice Pellets":
+            case "Heavy Thunderstorms and Ice Pellets":case "Thunderstorms and Ice Pellets":
+            case "Light Thunderstorms with Hail":case "Heavy Thunderstorms with Hail":case "Thunderstorms with Hail":
+            case "Light Thunderstorms with Small Hail":case "Heavy Thunderstorms with Small Hail":
+            case "Thunderstorms with Small Hail":
+                gifInputStream = getContext().getResources().openRawResource(+R.drawable.stormy);
+                break;
+            default:
+                gifInputStream = getContext().getResources().openRawResource(+R.drawable.sunny);
+        }
         gifMovie = Movie.decodeStream(gifInputStream);
     }
 
